f82e3c5bf00035f20b92a212c873bd0a183e8c82
haitran <mrtracy2011@gmail.com>
2016-09-18 23:09:26 +0700

    hai1
    hai2
    
    hai3
    
    hai4
    
    hai5
    
    loc1

--- DemoNew/ViewController.swift
+++ DemoNew/ViewController.swift
@@ -14,6 +14,30 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func hai1()  {
+        
+    }
+    func hai2()  {
+        
+    }
+    func hai3()  {
+        
+    }
+    func hai4()  {
+        
+    }
+    func hai5()  {
+        
+    }
+    
+    func loc1()  {
+        
+    }
+
+    
+
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
